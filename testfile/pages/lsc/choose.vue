dc7e3a9a0f864f633154f13a77352c62cee006ab
freebeebee <1375268220@qq.com>
2024-04-26 17:02:28 +0800

    又改了一下

--- testfile/pages/lsc/choose.vue
+++ testfile/pages/lsc/choose.vue
@@ -5,100 +5,111 @@
 		<text>1~5 的认识和加减法</text>
 	</view>
 	<ul class="listall">
-	<li class="choice">
-		<text class="intro">5以内的数比大小</text>
+	<li class="choice" v-for="(item,index) in questions">
+		<text class="intro">{{item}}</text>
 		<text class="ex_intro">例题：5 〇 4</text>
 		<text class="ex_intro" style="margin-left: 250rpx;">已练：4次</text>
 		<view class="circle">
 			<image src="../../static/三角形.png"></image>
 		</view>
 	</li>
-	<li class="choice">
-		<text class="intro">5以内的加法</text>
-		<text class="ex_intro">例题：3+1</text>
-		<text class="ex_intro" style="margin-left: 250rpx;">已练：2次</text>
-		<view class="circle">
-			<image src="../../static/三角形.png"></image>
-		</view>
-	</li>
-	<li class="choice">
-		<text class="intro">5以内的减法</text>
-		<text class="ex_intro">例题：5-3</text>
-		<text class="ex_intro" style="margin-left: 250rpx;">已练：1次</text>
-		<view class="circle">
-			<image src="../../static/三角形.png"></image>
-		</view>
-	</li>
-	<li class="choice">
-		<text class="intro">0的认识和加减法</text>
-		<text class="ex_intro">例题：0+5</text>
-		<text class="ex_intro" style="margin-left: 250rpx;">已练：3次</text>
-		<view class="circle">
-			<image src="../../static/三角形.png"></image>
-		</view>
-	</li>
-	</ul>
 	
-	<!--6~10 的认识和加减法-->
-	<view class="unit">
-		<image src="../../static/旗子.png"></image>
-		<text>6~10 的认识和加减法</text>
-	</view>
-	<ul class="listall">
-	<li class="choice">
-		<text class="intro">10以内的数比大小</text>
-		<text class="ex_intro">例题：7 〇 4</text>
-		<text class="ex_intro" style="margin-left: 250rpx;">已练：4次</text>
-		<view class="circle">
-			<image src="../../static/三角形.png"></image>
-		</view>
-	</li>
-	<li class="choice">
-		<text class="intro">6和7的加减法</text>
-		<text class="ex_intro">例题：6+1</text>
-		<text class="ex_intro" style="margin-left: 250rpx;">已练：2次</text>
-		<view class="circle">
-			<image src="../../static/三角形.png"></image>
-		</view>
-	</li>
-	<li class="choice">
-		<text class="intro">8和9的加减法</text>
-		<text class="ex_intro">例题：8-3</text>
-		<text class="ex_intro" style="margin-left: 250rpx;">已练：1次</text>
-		<view class="circle">
-			<image src="../../static/三角形.png"></image>
-		</view>
-	</li>
-	<li class="choice">
-		<text class="intro">10的加减法</text>
-		<text class="ex_intro">例题：5+5</text>
-		<text class="ex_intro" style="margin-left: 250rpx;">已练：3次</text>
-		<view class="circle">
-			<image src="../../static/三角形.png"></image>
-		</view>
-	</li>
-	<li class="choice">
-		<text class="intro">6~10的加减法</text>
-		<text class="ex_intro">例题：10-7</text>
-		<text class="ex_intro" style="margin-left: 250rpx;">已练：3次</text>
-		<view class="circle">
-			<image src="../../static/三角形.png"></image>
-		</view>
-	</li>
-	<li class="choice">
-		<text class="intro">连加</text>
-		<text class="ex_intro">例题：1+3+5</text>
-		<text class="ex_intro" style="margin-left: 250rpx;">已练：1次</text>
-		<view class="circle">
-			<image src="../../static/三角形.png"></image>
-		</view>
-	</li>
 	</ul>
+
 	<view style="height: 100rpx;"></view>
 </template>
 
 <script>
-	
+	export default{
+		mounted(){
+			console.log(this.questions)
+		},
+		data(){
+			return{
+				stage:0,
+				
+			
+				dis:[["5比大小"
+					,"5加法"
+					,"5减法"
+					,"0加减"
+					,"10比大小"
+					,"10加法"
+					,"10减法"
+					,"10加减"
+					,"10连加减"
+					,"10进位加法"
+					,"20加减"
+					,"20比大小"
+					,"20加减法比大小"
+					],
+					["100比大小"
+,"整十数加减"
+,"两位数加整十数"
+,"两位数加一位数"
+,"两位数加一位数（进位）"
+,"两位数减一位数"
+,"两位数减一位数（退位）"
+,"两位数加减一位数混合"
+,"100以内加减法比大小"
+],
+["100不进位加法"
+,"100不退位减法"
+,"100进位加法"
+,"100退位减法"
+,"100连加减"
+,"2-5乘法"
+,"10乘法"
+,"乘法加减法混合"
+],
+["表内除法"
+,"100加减混合运算（无括号）"
+,"100加减混合运算（有括号）"
+,"100乘除混合运算（无括号）"
+,"100乘除混合运算（有括号）"
+,"两级混合运算"
+],
+["两位数加两位数"
+,"两位数减两位数"
+,"末尾有0的三位数加法"
+,"末尾有0的三位数减法"
+,"三位数不进位加法"
+,"三位数进位加法"
+,"三位数不退位减法"
+,"三位数退位减法"
+,"整十、整百乘法"
+,"末尾有0的三位数乘一位数"
+,"中间有0的三位数乘一位数"
+,"两位数乘一位数（不进位）"
+,"两位数乘一位数（进位）"
+,"多位数乘一位数"
+,"分数比大小"
+,"分数加减"
+,"分数加减法比大小"
+],
+[
+	"两位数除以一位数"
+	,"末尾有0的三位数除以一位数"
+	,"三位数除以一位数"
+	,"商中间有0的除法"
+	,"商尾数有0的除法"
+	,"除数为一位数"
+	,"一位小数比大小"
+
+]
+			]	
+			}
+		},
+		computed:{
+			questions:{
+				get: function(){
+					return this.dis[5]
+				}
+				
+			}
+			
+		}
+	}
 </script>
 
 <style>
